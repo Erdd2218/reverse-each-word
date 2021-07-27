ea2956dc49f3214737a83702638d715ccc7e9304
erdd2218 <github email address>
2021-07-27 02:15:29 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
-def reverse_each_word(sentence)
-    sentence.split.collect do |word| 
-        word.reverse.join(" ")
-    end 
-end 
+
 
 def reverse_each_word(str)
     str.split.collect {|rev| rev.reverse!}.join(" ")
